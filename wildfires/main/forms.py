0be0be0a58733dd22b7b13a6c094ae43c966eb94
Andrew-Olmos <82090004+Andrew-Olmos@users.noreply.github.com>
2022-12-05 14:18:20 -0800

    Added Year Select Field

--- wildfires/main/forms.py
+++ wildfires/main/forms.py
@@ -16,5 +16,10 @@ class SearchForm(FlaskForm):
     ('OH', 'Ohio'), ('OK', 'Oklahoma'), ('OR', 'Oregon'), ('PA', 'Pennsylvania'), ('PR', 'Puerto Rico'), ('RI', 'Rhode Island'),('SC', 'South Carolina'), 
     ('SD', 'South Dakota'), ('TN', 'Tennessee'), ('TX', 'Texas'), ('UT', 'Utah'), ('VA', 'Virginia'), ('VI', 'Virgin Islands'),('VT', 'Vermont'), 
     ('WA', 'Washington'), ('WI', 'Wisconsin'), ('WV', 'West Virginia'), ('WY', 'Wyoming')]
+    # 1992 - 2015
+    yearChoices = [('1992', '1992'), ('1993', '1993'), ('1994', '1994'), ('1995', '1995'), ('1996', '1996'), ('1997', '1997'), ('1998', '1998'), ('1999', '1999'),
+    ('2000', '2000'),('2001', '2001'), ('2002', '2002'), ('2003', '2003'), ('2004', '2004'), ('2005', '2005'), ('2006', '2006'), ('2007', '2007'), ('2008', '2008'), 
+    ('2009', '2009'), ('2010', '2010'), ('2011', '2011'), ('2012', '2012'), ('2013', '2013'), ('2014', '2014'), ('2015', '2015')]
+    year = SelectField(u"Year", choices = yearChoices, validators=[DataRequired()])
     state = SelectField(u"State", choices = stateChoices, validators = [DataRequired()])
     submit = SubmitField("Search")
